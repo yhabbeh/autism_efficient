1204c3eba2b0f8664710b78307f3ec1a76dde505
yousef habbeh <67245907+yhabbeh@users.noreply.github.com>
2024-03-09 12:17:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,20 @@
-import cv2
-from flask import Flask, request
-from flask_cors import CORS
+from flask import Flask, request, jsonify
+import tensorflow as tf
 import numpy as np
 from io import BytesIO
-import tensorflow as tf
 from PIL import Image
-from skimage import transform
 from keras.models import load_model
-from flask import request, jsonify
+from skimage import transform
+import cv2
+
+
 
 app = Flask(__name__)
-CORS(app)
-
+model = load_model('inception_model.h5')
 
 @app.route('/')
 def helloworld():
     return 'Prediction Home Page'
-
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files.getlist('images[]')
-        print(file)
-        # for imagesingle in f:
-        #     image = Image.open(BytesIO(imagesingle.read()))
-        #     print(predict(face_crop_image(image), model))
-        # if predict(face_crop_image(image), model):
-        #     return {"response": "the user has an autism", "result": "True"}
-        # else:
-        #     return {"response": "the user hasn't an autism", "result": "False"}
-        # return send_from_direcaory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
-
-
-def multi_images_predict(filenames):
-    counter = 0
-    count_detect = 0
-    for i in filenames:
-        print(counter)
-        if prediction(face_crop_image(Image.open(BytesIO(i.read()))), model_autism=model):
-            count_detect += 1
-        counter += 1
-    return count_detect / counter
-
 
 @app.route('/uploads', methods=['POST', 'GET'])
 def upload_files():
@@ -52,21 +24,24 @@ def upload_files():
         files = request.files.getlist('images[]')
         result = multi_images_predict(files)
         if result >= .5:
-            return {"response": "the user has an autism", "result": "True", "precentage": result}
+            print("result", "True", "precentage", result)
+            return jsonify({"response": "the user has an autism", "result": True, "precentage": result}), 200
         else:
-            return {"response": "the user hasn't an autism", "result": "False"}
+            print("result", "False", "precentage", result)
+            return jsonify({"response": "the user hasn't an autism", "result": False, "precentage": result}), 200
     else:
         return jsonify({"Error 405 ": "The method is not allowed for the requested URL this API is \'POST\'"}), 405
 
 
-def prediction(image, model_autism):
-    np_image = np.array(image).astype('float32') / 255
-    np_image = transform.resize(np_image, (224, 224, 3))
-    np_image = np.expand_dims(np_image, axis=0)
-    img = tf.image.resize(np_image, (224, 224))  # Resize the image
-    res = model_autism.predict(img)[0].argmax()
-    print(res)
-    return res == 1
+def multi_images_predict(files):
+    counter = 0
+    count_detect = 0
+    for image in files:
+        print('counter =>', counter)
+        if prediction(face_crop_image(image=Image.open(BytesIO(image.read()))), model_autism=model):
+           count_detect += 1
+        counter += 1
+    return count_detect / counter
 
 
 def face_crop_image(image):
@@ -81,8 +56,11 @@ def face_crop_image(image):
         return face
     return image
 
+def prediction(image, model_autism):
+    np_image = np.array(image).astype('float32') / 255
+    np_image = transform.resize(np_image, (224, 224, 3))
+    np_image = np.expand_dims(np_image, axis=0)
+    img = tf.image.resize(np_image, (224, 224))  # Resize the image
 
-if __name__ == '__main__':
-    model = load_model('inception_model.h5')
-    app.config['UPLOAD_FOLDER'] = 'uploads'
-    app.run(host='0.0.0.0', port=5000)
+    res = model_autism.predict(img)[0].argmax()
+    return res == 1
